acb87272d07a02daa0d108de1013769787ad2f22
Laura Kauria <laura.kauria@gmail.com>
2020-05-27 16:47:56 +0300

    adding columns to opponent class

--- application/opponents/models.py
+++ application/opponents/models.py
@@ -7,6 +7,9 @@ class Opponent(db.Model):
     onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
+    year_of_birth = db.Column(db.Integer, nullable=True)
+    strengths = db.Column(db.String(10000), nullable=True)
+    weaknesses = db.Column(db.String(10000), nullable=True)
 
     def __init__(self, name):
         self.name = name
