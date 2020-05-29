8c70f6feefd8d05a9b43ec2c0bbc27ee2fbd9ffc
Laura Kauria <laura.kauria@gmail.com>
2020-05-29 13:26:37 +0300

    opponents_create(): --> adding parameters in a more concise way

--- application/opponents/views.py
+++ application/opponents/views.py
@@ -2,6 +2,7 @@ from application import app, db
 from flask import redirect, render_template, request, url_for
 from application.opponents.models import Opponent
 from application.opponents.forms import OpponentForm
+from operator import itemgetter
 
 @app.route("/opponents", methods=["GET"])
 def opponents_index():
@@ -12,19 +13,23 @@ def opponents_form():
     return render_template("opponents/new.html", form = OpponentForm())
   
 @app.route("/opponents/<opponents_id>/", methods=["POST"])
-def opponents_set_done(opponents_id):
+def opponents_change_values(opponents_id):
 
     t = Opponent.query.get(opponent_id)
+    #example:  --> t.done = True
     db.session().commit()
   
     return redirect(url_for("opponents_index"))
 
+
+
 @app.route("/opponents/", methods=["POST"])
 def opponents_create():
-    t = Opponent(request.form.get("name"),
-    request.form.get("year_of_birth"), 
-    request.form.get("strengths"), 
-    request.form.get("weaknesses"))
+
+    
+    getter = itemgetter("name", "year_of_birth", "strengths", "weaknesses")
+    values = getter(request.form)
+    t = Opponent(*values)
 
     db.session().add(t)
     db.session().commit()
