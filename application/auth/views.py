f3ec442c5142cbd6470e05f139f0de84629ef5a8
Laura Kauria <laura.kauria@gmail.com>
2020-05-31 12:59:24 +0300

    adding LoginForm

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,6 +1,6 @@
-
 from flask import render_template, request, redirect, url_for
-
+from flask_login import login_user
+  
 from application import app
 from application.auth.models import User
 from application.auth.forms import LoginForm
@@ -16,8 +16,8 @@ def auth_login():
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
         return render_template("auth/loginform.html", form = form,
-                               error = "No such username or password")
+                                error = "No such username or password")
 
 
-    print("Käyttäjä " + user.name + " tunnistettiin")
+    login_user(user)
     return redirect(url_for("index"))    
