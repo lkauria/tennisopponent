a91d8939141dfe7f6bdea4b3b933978cfdf50b98
Laura Kauria <laura.kauria@gmail.com>
2020-05-31 17:02:38 +0300

    adding log out feature

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user
   
 from application import app
 from application.auth.models import User
@@ -11,13 +11,16 @@ def auth_login():
         return render_template("auth/loginform.html", form = LoginForm())
 
     form = LoginForm(request.form)
-    # mahdolliset validoinnit
-
+    # probable validations
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
         return render_template("auth/loginform.html", form = form,
                                 error = "No such username or password")
 
-
     login_user(user)
     return redirect(url_for("index"))    
+
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
+    return redirect(url_for("index")) 
